935ec81e123f879275ef09546fb86f8daa6bcfe5
Aqilafadilla25 <aqila24ti@mahasiswa.pcr.ac.id>
2025-10-13 23:36:26 +0700

    progress hari inu

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\MahasiswaController;
 
 use App\Http\Controllers\MatakuliahController;
 
+use App\Http\Controllers\DashboardController;
+
+
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
